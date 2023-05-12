fd4a5f4ade64c022428174d2dabdca242be9c379
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-05-12 17:34:27 +0200

    Added partInt for distanceOfTicket const

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
-const distanceOfTicket = prompt('scrivi la distanza del tuo viaggio');
+const distanceOfTicket = parseInt (prompt('scrivi la distanza del tuo viaggio in km'));
 const userAge = prompt('scrivi la tua eta');
 const priceOfTicket = 0.21

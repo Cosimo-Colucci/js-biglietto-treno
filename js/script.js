a33dc48fbf3d8e0fa5a7d6468d5ea27afc3cfc13
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-05-12 17:47:07 +0200

    created console and document display with multip

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 const distanceOfTicket = parseInt (prompt('scrivi la distanza del tuo viaggio in km'));
 const userAge = prompt('scrivi la tua eta');
 const priceOfTicket = 0.21
+
+console.log(distanceOfTicket * priceOfTicket) ;
+
+document.getElementById('price').innerHTML = (distanceOfTicket * priceOfTicket) ;
